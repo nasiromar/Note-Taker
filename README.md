34644a46e7776cac317b4b4d537e4e71ad161f93
Mona Lisa <nasiromar321@gmail.com>
2022-11-26 20:11:05 -0600

    readme

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Note-Taker
+# Note Taker  
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)  
+## Description
+
+[Deployed link](https://github.com/nasiromar/Note-Taker)
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution](#contribution)
+* [Tests](#tests)
+* [Credits](#credits)
+* [License](#license)
+* [Questions](#questions)
+## Installation
+go to the command line and run server.js and npm.
+## Usage
+The app has a get started page which when pressed will take you the official note taking page where you can save and  take new notes. the repo  displays all the things that were needed to make this app run accordingly and also it gives information on step by step action on what is taking place .
+![Project Screenshot](/db/555.png)
+## Contribution
+please reach out to me at email for any comments and concerns 
+## Tests
+no testing 
+## Credits
+none.
+## License
+[MIT License](https://choosealicense.com/licenses/mit/)
+
+## Questions
+If you have questions, please reach out via email or my GitHub account.
+* GitHub: [nasiromar321](https://github.com/nasiromar321)
+* E-mail: [nasiromar321@gmail.com](mailto:nasiromar321@gmail.com)
+    
--- server.js
+++ server.js
@@ -1,36 +1,55 @@
 const express = require('express');
 const path = require('path');
-const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
+const fs = require('fs');
+//const { clog } = require('./middleware/clog');
+//const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+const publicDir = path.join(__dirname, "/public");
 
 // Import custom middleware, "cLog"
-app.use(clog);
+//app.use(clog);
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+//app.use('/api', api);
 
 app.use(express.static('public'));
 
 // GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(publicDir, '/notes.html'))
 );
 
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+app.get('/api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './db/db.json'))
 );
 
-// Wildcard route to direct users to a 404 page
+app.get('/api/notes/id', (req, res) => {
+    let notes = JSON.parse (fs.readFileSync("./db/db.json", 'utf8'));
+    res.json(notes[Number(req.params.id)])
+}
+);
+
+
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+  res.sendFile(path.join(publicDir, 'index.html'))
 );
+
+app.post("/api/notes", function (req, res) {
+    let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let note = req.body;
+    let newId = notes.length.toString();
+    note.id = newId;
+    notes.push(note);
+  
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+    console.log("Note saved to db.json. Content: ", note);
+    res.json(notes);
+  });
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
